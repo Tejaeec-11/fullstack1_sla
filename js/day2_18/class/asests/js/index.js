ad1fd0c103d952429979169cc50dc0fe373f1fd9
Tejeswari J <jtejashwari@gmail.com>
2026-09-07 14:27:34 +0530

    Initial commit

---
+++ js/day2_18/class/asests/js/index.js
@@ -0,0 +1,102 @@
+// ; not nesscary 
+console.log("this is number ") //string use
+
+console.log(4567) //number or "4567" is string value
+
+var a; //it retun undefined it is called decalaration.but move to next line
+console.log(a) //initazion to 10 
+ 
+var b = 10; // it is called inization and expression
+console.log(b)
+
+
+
+//increment
+var tejCap = 20
+tejCap--
+console.log(tejCap)
+
+//print type only me conso.log
+"this is text 757657567" //we cant see in output its in system but see use consol.log()
+
+//string 
+console.log(4567.765647)// for number use string or not output we see in consol.log 
+console.log("react") // but for words we need use string for not init only display plain desiplay this we used string or else not see the output we (react) instead ("react")
+
+
+
+
+var studentName =  true;
+console.log(studentName); //underfined
+
+let studentName = true;
+console.log(studentName);        // true
+console.log(typeof studentName); // "boolean"
+
+let studentName = false;
+console.log(studentName);        // false
+console.log(typeof studentName); // "boolean"
+
+
+let studentName = "true";        // This is a STRING, not boolean!
+console.log(studentName);        // "true"
+console.log(typeof studentName); // "string"
+
+let studentName = "false";       // This is a STRING, not boolean!
+console.log(studentName);        // "false"
+console.log(typeof studentName); // "string"
+
+typeof "Hello"          // "string"
+typeof 42               // "number"
+typeof true             // "boolean"
+typeof undefined        // "undefined"
+typeof null             // "object" (historical bug in JS)
+typeof {}               // "object"
+typeof []               // "object" (array is object)
+typeof function(){}     // "function"
+
+
+
+let a = 10;
+let b = a;    // Copy value
+b = 20;
+console.log(a); // 10 (unchanged)
+console.log(b); // 20
+
+
+let obj1 = { name: "Teju" };
+let obj2 = obj1;        // Copy reference (same object)
+obj2.name = "Tejeswari";
+
+console.log(obj1.name); // "Tejeswari" (changed!)
+console.log(obj2.name); // "Tejeswari"
+
+let name = "Tejeswari";   // String
+let age = 22;             // Number
+let isStudent = true;     // Boolean
+let city;                 // Undefined
+let salary = null;        // Null
+
+// Object
+let person = {
+    name: "Tejeswari",
+    age: 22,
+    city: "Chennai"
+};
+
+// Array
+let colors = ["Red", "Blue", "Green"];
+
+// Function
+function greet() {
+    console.log("Hello!");
+}
+
+
+
+
+// boolean
+console.log(True); // code stop becasue true is not defined  because string not put if e put sting its show in output 
+console.log("run") // 18 line error so next line not come in output it like block
+
+
---
+++ js/day3_19/class/asests/js/index.js
@@ -0,0 +1,74 @@
+let a //decalartion,statement
+console.log(a)
+console.log("new")
+
+
+let b = 10 //statement , varaivale initalization , expression
+console.log(b) //to see in output
+console.log("new")
+
+
+//type 1 - only  statement ,printing
+console.log("this is my data ")
+console.log(10+50)
+console.log(10*20 == 200) //10*20=200 and 200 == is a check if both same true if not false
+console.log("new")
+
+
+//type2 -inization,statement,expression,i can reasign
+let c = "this is my data"
+console.log(c)
+
+
+
+//operators
+console.log(1)
+let d = 10;
+console.log(d)
+
+console.log(2)
+e=10 
+f =+ e
+console.log(f)
+
+// let a=10
+//let b,data +=20 means b+20 means b is not initz
+//console.log(b)
+//or 
+console.log(2.1)
+let p =10
+console.log(p+=10) //p=p+10
+
+
+console.log(3) //same as 1,2
+let g=10 
+g =g  //g=g+20//=+g give value in no or give g=g
+//g=+20 //try this important
+console.log(g)
+
+//declare
+
+console.log("d1")
+var v 
+console.log(v)
+
+ 
+console.log("d2")
+let q 
+console.log(q)
+
+//var const //const 1st init 
+//console.log(v)
+
+
+//reassign var, let ,const not 
+
+
+let y=10
+//let l =+ 30 does not want this..reassign panra area we havve = athu venam but you y change itself we used consol.log y+30
+console.log(y+30) //this answer
+
+//const reasign
+const z=10
+const u=z+20 //used onther variable to change value 
+console.log(u)
