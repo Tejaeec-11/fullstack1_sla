819c0e8689eb7ad29212b4143f8625da589dfac6
Tejeswari J <jtejashwari@gmail.com>
2026-09-17 20:35:30 +0530

    day20class

---
+++ react/day3_17/class/designpractice/src/App.jsx
@@ -0,0 +1,11 @@
+const App = ()=>{
+  return (
+    <>
+    <navbar/>
+    <div>
+     <h1>this is data </h1>
+    </div>
+    </>
+  )
+}
+export default App
---
+++ react/day3_17/class/designpractice/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const navbar = ()=>{
+    return (
+        <>
+        <div>
+            <a href="">Home</a>
+             <a href="">about</a>
+              <a href="">contact</a>
+               <a href="">Help</a>
+
+        </div>
+        
+        </>
+    )
+}
