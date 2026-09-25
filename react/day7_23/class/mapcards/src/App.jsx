60241812eb77590be59c036e513422e2ca7bcd11
Tejeswari J <jtejashwari@gmail.com>
2026-09-25 18:54:49 +0530

    reset

--- react/day7_23/class/mapcards/src/App.jsx
+++ react/day7_23/class/mapcards/src/App.jsx
@@ -1,32 +1,58 @@
-
+import { useState } from "react";
 
 const App = () => {
-  const studentData = [
-    { name: "tej", age: 20, email: "tej@gmail.com", course: "css" },
-    { name: "tej", age: 20, email: "tej@gmail.com", course: "css" },
-    { name: "tej", age: 20, email: "tej@gmail.com", course: "css" },
-    { name: "tej", age: 20, email: "tej@gmail.com", course: "css" },
-  ];
+  const [nameuser, setNameUser] = useState("");
+  const [ageuser, setAgeUser] = useState("");
+  const [showData, setShowData] = useState([]);
+
+  const handlechange = (e) => {
+    setNameUser(e.target.value);
+  };
+
+  const handlechangeage = (e) => {
+    setAgeUser(e.target.value);
+  };
+
+  const handleclick = () => {
+    const obj = {
+      id: Date.now(),
+      name: nameuser,
+      age: ageuser
+    };
+
+    const arr = [...showData];
+    arr.push(obj);
+    setShowData(arr);
+
+    alert("sucessfull saved");
+    setNameUser("");
+    setAgeUser("");
+  };
 
   return (
     <>
-      <div className="bg-blue-400 min-h-screen flex justify-between p-5 gap-5">
-        {studentData.map((e, i) => (
-          <div
-            key={i}
-            className="bg-white p-5 w-64 h-60 rounded-lg"
-          >
-            <h2>Name: {e.name}</h2>
-            <h2>Age: {e.age}</h2>
-            <h2>Email: {e.email}</h2>
-            <h2>Course: {e.course}</h2>
+     
+      <input
+        type="text"
+        onChange={handlechange}
+        placeholder="Enter the name"
+      />
 
-            <button className="bg-black text-white p-2 mt-4 rounded">
-              View
-            </button>
-          </div>
-        ))}
-      </div>
+      <input
+        type="number"
+        onChange={handlechangeage}
+        placeholder="Enter the age"
+      />
+     
+
+      <button onClick={handleclick}>click to login</button>
+
+      {showData.map((item) => (
+        <div key={item.id}>
+          <p>Name: {item.name}</p>
+          <p>Age: {item.age}</p>
+        </div>
+      ))}
     </>
   );
 };
--- react/day7_23/class/mapcards/vite.config.js
+++ react/day7_23/class/mapcards/vite.config.js
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
+//  import tailwindcss from '@tailwindcss/vite'
+//tailwindcss()
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),],
 })
